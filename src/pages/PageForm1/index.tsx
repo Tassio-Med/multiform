1a4761954f86f4f38768ba053fea1bbe91cca4e0
Tassio-Med <tassiohfmed@gmail.como>
2023-08-24 16:53:50 -0300

    feat: pageForm1 estruturado

--- src/pages/PageForm1/index.tsx
+++ src/pages/PageForm1/index.tsx
@@ -1,9 +1,33 @@
 import { Theme } from "../../components/Theme"
+import * as F from "./styles";
+import { useNavigate } from "react-router-dom";
 
 export const PageForm1 = () => {
+  const navigate = useNavigate();
+
+  const handleNextStep = () => {
+    navigate('/page2')
+  }
+
   return(
     <Theme>
-      <h1>Página 01</h1>
+      <F.Container>
+        <p> Passo 1/3</p>
+        <h1>Queremos muito te conhecer</h1>
+        <p>Preencha o campo abaixo com o seu nome completo.</p>
+
+        <hr />
+
+        <label>
+          Seu nome completo
+          <input
+            type="text"
+            autoFocus  
+          />
+
+          <button onClick={handleNextStep}>Próximo</button>
+        </label>
+      </F.Container>
     </Theme>
   )
 }
