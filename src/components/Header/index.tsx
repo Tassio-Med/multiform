82546d49a12d4a67537f67d4d3cc343eb5b3dc86
Tassio-Med <tassiohfmed@gmail.como>
2023-08-24 11:53:18 -0300

    feat: header estruturado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
 export const Header = () => {
   return(
     <div>
-      <h1>AQUI É O HEADER</h1>
+      <h1>Banco de Talentos</h1>
+      <h3>Preencha seus dados e encontre a tão sonhada vaga!</h3>
     </div>
   )
 }
