6846c1874ca66fe05d01d74f78feaa52965d2a8b
Tassio-Med <tassiohfmed@gmail.como>
2023-08-28 17:47:48 -0300

    feat: página dois configurada

--- src/pages/PageForm2/index.tsx
+++ src/pages/PageForm2/index.tsx
@@ -1,11 +1,69 @@
+import { useEffect } from "react";
+import { Theme } from "../../components/Theme"
+import { useFormContext } from "../../contexts/useFormContext";
 import * as F from "./styles";
-import { Theme } from "../../components/Theme";
+import { useNavigate, Link } from "react-router-dom";
+import { FormActions } from "../../types";
+import { SelectOption } from "../../components/SelectOption";
 
 export const PageForm2 = () => {
+  const {state, dispatch } = useFormContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2
+    });
+  });
+  
+
+  const handleNextStep = () => {
+    state.name !== "" ? navigate('/page3') : alert("Preenha o seus dados!");
+  }
+  
+  const setLevel = (level: number) => {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: level
+    });
+  }
+
   return(
     <Theme>
       <F.Container>
-        Página 02
+        <p> Passo 2/3 - {state.currentStep}</p>
+        <h1>{state.name}, agora selecione o seu nível.</h1>
+        <p>Preencha o campo abaixo com o seu nome completo.</p>
+
+        <hr />
+
+        <SelectOption
+          isSelected={state.level === 0}
+          icon = "🥳"
+          title = "Júnior"
+          description = "Sou desenvolvedor com dois anos de mercado"
+          onClick = {() => setLevel(0)}
+        />
+
+        <SelectOption
+          isSelected={state.level === 1}
+          icon = "😎"
+          title = "Pleno"
+          description = "Sou desenvolvedor com um três ou mais anos dde mercado"
+          onClick = {() => setLevel(1)}
+        />
+
+        <SelectOption
+          isSelected={state.level === 2}
+          icon = "🥸"
+          title = "Sênior"
+          description = "Sou desenvolvedor com um cinco ou mais anos dde mercado"
+          onClick = {() => setLevel(2)}
+        />
+
+        <Link to="/">Voltar</Link>
+        <button onClick={handleNextStep}>Próximo</button>
       </F.Container>
     </Theme>
   )
