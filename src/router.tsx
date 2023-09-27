f5d223ec438cafac55879c91e297e70d7252b2f7
Tassio-Med <tassiohfmed@gmail.como>
2023-09-27 15:53:06 -0300

    feat: BrowserRouter adicionado à main para nõ ter erro nos testes

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { PageForm1 } from "./pages/PageForm1";
 import { PageForm2 } from "./pages/PageForm2";
 import { PageForm3 } from "./pages/PageForm3";
@@ -6,13 +6,11 @@ import { PageForm4 } from "./pages/PageForm4";
 
 export const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={ PageForm1 }/>
-        <Route path="/page2" Component={ PageForm2 }/>
-        <Route path="/page3" Component={ PageForm3 }/>
-        <Route path="/page4" Component={ PageForm4 }/>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" Component={ PageForm1 }/>
+      <Route path="/page2" Component={ PageForm2 }/>
+      <Route path="/page3" Component={ PageForm3 }/>
+      <Route path="/page4" Component={ PageForm4 }/>
+    </Routes>
   )
 }
