8c162dd494b865023f6622b8c7a70b1e83740d07
Tassio-Med <tassiohfmed@gmail.como>
2023-09-28 20:01:28 -0300

    test: arquivo criado para adicionar s testes das rotas

---
+++ src/__tests__/routes/renderRoutes.test.tsx
@@ -0,0 +1,15 @@
+// src/App.test.tsx
+
+// import { screen } from '@testing-library/react';
+// import { renderWithRouter } from "../utils/__mocks__/renderWithRouter";
+// import App from '../../App';
+
+// it('Navega para a página PageForm2', async () => {
+//   const { user } = renderWithRouter(<App />);
+
+//   expect(screen.getByText(/Queremos muito te conhecer/i)).toBeInTheDocument();
+
+//   const nextLink = screen.getByRole('navigate', { name: /Próximo/i });
+//   await user.click(nextLink);
+//   expect(screen.getByText(/Selecione o campo que corresponde ou chegue mais perto do seu tempo na área de tecnologia./i)).toBeInTheDocument();
+// });
